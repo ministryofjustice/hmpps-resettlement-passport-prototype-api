42e8dc42bdb2c3b850a7ce27681095c9c8fc4d27
Abdul <abdul.rahuman@digital.justice.gov.uk>
2023-05-30 09:40:43 +0100

    Prison API Integration proof of concept

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportprototypeapi/HmppsResettlementPassportPrototypeApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportprototypeapi/HmppsResettlementPassportPrototypeApi.kt
@@ -3,6 +3,11 @@ package uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+/**
+ * Used as the system oauth username when calling HMPPS apis
+ * as well as the "audit module" for creating records in prison-api
+ */
+const val SYSTEM_USERNAME = "RESETTLEMENT_PASSPORT_API"
 @SpringBootApplication()
 class HmppsResettlementPassportPrototypeApi
 
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportprototypeapi/resource/PrisonResourceController.kt
@@ -0,0 +1,97 @@
+package uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.resource
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.config.ErrorResponse
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.data.prisonapi.Prison
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.service.PrisonApiService
+import java.lang.IllegalArgumentException
+
+
+@RestController
+@Validated
+@RequestMapping("/hmpps", produces = [MediaType.APPLICATION_JSON_VALUE])
+class PrisonResourceController (
+  private val prisonService: PrisonApiService,
+){
+
+
+  @GetMapping("/prisons")
+  @Operation(summary = "Get all prisons", description = "All prisons")
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successful Operation",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun getPrisons(): List<Prison> = prisonService.getPrisons(false)
+
+  @GetMapping("/prisons/id/{prisonId}")
+  @Operation(summary = "Get a prison", description = "A prison data")
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successful Operation",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun getPrisonbyId(
+    @PathVariable
+    prisonId: String): Prison =
+    prisonService.getPrisonById(true, prisonId)
+
+
+
+  @GetMapping("/prisons/id/{prisonId}/videolink-conferencing-centre/email-address")
+  @Operation(summary = "Get a prison Videolink Conferencing centre email address", description = "A prison Videlolink Conference center email address")
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successful Operation",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Data Not Found",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "500",
+        description = "Data Not Found",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  suspend fun getPrisonVideolinkConferenceCentreEmailAddress(
+    @PathVariable
+    prisonId: String): String  = prisonService.getPrisonVideolinkConferenceCentreEmailAddress(true, prisonId)
+}

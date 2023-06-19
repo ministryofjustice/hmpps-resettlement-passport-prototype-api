32b6bf77057d9ae9e1c7280883c49df428a4d254
Abdul <abdul.rahuman@digital.justice.gov.uk>
2023-06-19 14:44:58 +0100

    Update OffenderSearchResourceController.kt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportprototypeapi/resource/OffenderSearchResourceController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportprototypeapi/resource/OffenderSearchResourceController.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.resource
 
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -10,8 +9,8 @@ import org.springframework.http.MediaType
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.data.prisonapi.OffenderSearchPrisoner
@@ -42,14 +41,13 @@ class OffenderSearchResourceController(
   suspend fun getPrisonerbyTerm(
     @PathVariable
     prisonId: String,
-    @RequestBody
-    @Parameter
+    @RequestParam
     term: String,
-    @Parameter
+    @RequestParam
     page: Int,
-    @Parameter
+    @RequestParam
     size: Int,
-    @Parameter
+    @RequestParam
     sort: String,
   ): List<OffenderSearchPrisoner> = offenderSearchService.getPrisonersByTerm(prisonId, term, page, size, sort)
 }

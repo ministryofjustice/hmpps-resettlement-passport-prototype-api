13c119e76a8228560bd5962cb55bebf7822ad2ea
abdulrkalam <129852408+abdulrkalam@users.noreply.github.com>
2023-06-22 10:14:05 +0100

    Dev (#32)
    * API Sample Check
    
    * Validate Check fixes
    
    * Update Dockerfile
    
    commenting the DB config for now
    
    * build fix
    
    * build fix
    
    * build fix
    
    * build fix
    
    * Update SampleResourceController.kt
    
    * Update docker-compose.yml
    
    build fix for  /gradlew --no-daemon assemble error
    
    * fix for the deploy error
    
    * check add the install
    
    * RDS enabled
    
    * RDS changes reverted
    
    * Update docker-compose.yml
    
    * RDS Enabled
    
    * Prison API Integration proof of concept
    
    * Prison API with credentials
    
    * Modified to use the direct UR for nowL instead of docker image
    
    * Update build.gradle.kts
    
    * Build Fix for test units
    
    * build fix for check validation
    
    * build fix for check validation
    
    * build fix for check validation
    
    * Removing the container for auth and prison api
    
    * commenting the test temporarily to validate the deployment
    
    * Empty-Commit
    
    * set environment variable for dev
    
    * Added offender-search url fix for deployment issue
    
    * r2dbc url added for deployment
    
    * add dev db values
    
    * Fix for swagger
    
    * Flyway enabled for db migration
    
    * Fix for Build in DEV
    
    * Empty-Commit
    
    * flyway fix
    
    * Update V1_1__start.sql
    
    * Empty-Commit
    
    * Update ResourceServerConfiguration.kt
    
    * Unit test enabled
    
    * fix for the gradle build check
    
    * Unit Test for Integration enabled
    
    * DNS Name change
    
    * Update OffendersSearchResourceContorller.kt
    
    * Update OffendersSearchResourceContorller.kt
    
    * Update OffenderSearchResourceController.kt
    
    * Update OffenderSearchResourceController.kt
    
    * Update values.yaml
    
    * Offender Search API Added
    
    * build fix for gradle check
    
    * Update OffendersDTO.kt
    
    * Update OffendersDTO.kt
    
    * Delete OffenderSearchPrisoner.kt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportprototypeapi/service/OffenderSearchApiService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsresettlementpassportprototypeapi/service/OffenderSearchApiService.kt
@@ -1,20 +1,51 @@
 package uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.service
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
-import uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.data.prisonapi.OffenderSearchPrisoner
+import uk.gov.justice.digital.hmpps.hmppsresettlementpassportprototypeapi.data.prisonapi.OffendersList
 
 @Service
 class OffenderSearchApiService(
   private val offenderSearchWebClient: WebClient,
 ) {
 
-  suspend fun getPrisonersByTerm(prisonId: String, term: String, page: Int, size: Int, sort: String): List<OffenderSearchPrisoner> {
-    return offenderSearchWebClient
-      .get()
-      .uri("/prison/{id}/prisoners?term=" + term + "&page=" + page + "&size=" + size + "&sort=" + sort)
+  fun findOffendersBySearchTerm(prisonId: String, term: String = "", pageable: Pageable): Flow<OffendersList> = flow {
+    // var page = 0
+    // do {
+    val pageOfData = offenderSearchWebClient.get()
+      .uri(
+        "/prison/{prisonId}/prisoners?term={term}&size={size}&page={page}&sort={sort}",
+        mapOf(
+          "prisonId" to prisonId,
+          "term" to term,
+          "size" to pageable.pageSize, // NB: API allows up 3,000 results per page
+          "page" to pageable.pageNumber,
+          "sort" to "prisonerNumber,ASC",
+        ),
+      )
       .retrieve()
-      .awaitBody()
+      .awaitBody<OffendersList>()
+    emit(pageOfData)
+    //  page += 1
+    //  } while (!pageOfData.last)
+  }
+
+  /**
+   * Searches for offenders in a prison using term firstName or LastName  (e.g. ARC)
+   * returning a complete list.
+   * Requires role PRISONER_IN_PRISON_SEARCH or PRISONER_SEARCH
+   */
+  suspend fun getOffendersBySearchTerm(prisonId: String, term: String = "", pageable: Pageable): Flow<OffendersList> {
+    /*val offenders = mutableListOf<Offenders>()
+    findOffendersBySearchTerm(prisonId, term, pageable).collect {
+      offenders.addAll(it)
+    }
+    return offenders
+    */
+    return findOffendersBySearchTerm(prisonId, term, pageable)
   }
 }
